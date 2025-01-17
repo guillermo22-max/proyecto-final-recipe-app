76080f269dd3c9826355d49391b46f03b7f4edd0
guillermo22-max <guillermosoto989@gmail.com>
2025-01-17 17:32:19 +0000

    modal para solicitar restablecimiento de contraseña, etc.

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('token');
-
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
     }
+});
+
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+  config.headers.Authorization = 'Bearer ' + token;
+  return config;
+}, function (error) {
+  return Promise.reject(error);
 });
 
 export default api;
